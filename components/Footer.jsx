3dedd132d3dcfcbf64b9dee6e9ddf6c669cee323
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2023-08-24 04:13:04 +0530

    Update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 DARKSIDE All rights reserverd</p>
+      <p>2023 Trendy Treasures All rights reserverd</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>DARKSIDE</title>
+        <title>Trendy Treasures </title>
       </Head>
       <header>
         <Navbar />
